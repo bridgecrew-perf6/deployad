e59ad93634c1d493770655915ca43099b83e486d
Mohamed Sabri <mohamed@datalyticsbusiness.com>
2020-11-29 12:20:17 -0500

    Update app.R

--- AnomalyDetection/app.R
+++ AnomalyDetection/app.R
@@ -16,7 +16,10 @@ library(xgboost)
 library(ids)
 library(scales)
 
+
 test_data <- readRDS("data/test.rds")
+
+table1 <- test_data[c("feature_1501", "feature_1511","feature_1514","feature_1520","feature_1527","feature_1533","feature_1534","feature_1540","feature_1542","Class")]
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -35,13 +38,13 @@ ui <- fluidPage(
                           Class - (0 or 1) - Represents Good/Anomalous class labels for the products"))),column(2)),
                
                hr(),
-               p(h3("Developer")),
-               p(a("Mohamed Sabri", href="https://www.linkedin.com/in/mohamed-sabri/", target="_blank"),style = "font-size:25px"),
-               hr(),
                fluidRow(
                  column(3),
                  column(5, imageOutput("intro")),
-                 column(3))
+                 column(3)),
+               hr(),
+               p(h3("Developer")),
+               p(a("Mohamed Sabri", href="https://www.linkedin.com/in/mohamed-sabri/", target="_blank"),style = "font-size:25px")
              ),
              tabPanel("Raw Data",DTOutput('raw')),
              tabPanel("Anomaly Detection",fluidPage(theme = shinytheme("flatly")),
@@ -50,12 +53,12 @@ ui <- fluidPage(
                       pageWithSidebar(
                         headerPanel('Parameter'),
                         sidebarPanel(width = 3,
-                                     selectInput('model', 'Select a model:',c('Model 1')),
+                                     selectInput('model', 'Select a model:',c('Model 1: XGBoost')),
                                      sliderInput("thres", "Threshold:",
-                                                 min = 50, max = 100,
-                                                 value = c(50,100)),
-                                     checkboxGroupInput(inputId = "show",
-                                                        label = 'Show status:', choices = c("Show status" = "status")),
+                                                 min = 1, max = 99,
+                                                 value = 25),
+                                     checkboxInput(inputId = "show",
+                                                        label = 'Show status', value = TRUE),
                                      actionButton("run","Run the simulation")
                         ),
                         mainPanel(
@@ -78,9 +81,12 @@ server <- function(input, output,session) {
     img_dec <- list("www/alert1.jpg","www/alert2.jpg","www/alert3.jpg","www/alert4.jpg")
     
     output$raw <- DT::renderDT({
-        colnames(test_data)[51:60] <- c("CP1489", "SensorTemp23","CP5632", "SensorBar78","AH1198", "SensorPres88","AH9923", "SensorBar11","Equip12", "Class")
         
-        datatable(test_data[,51:60]) 
+        colnames(table1)<- c("CP1489", "SensorTemp23","CP5632", "SensorBar78","AH1198", "SensorPres88","AH9923", "SensorBar11","Equip12", "Class")
+        
+        datatable(table1, options = list(
+          order = list(list(1, 'desc'))
+        )) 
     })
     
     output$intro <- renderImage({
@@ -88,39 +94,41 @@ server <- function(input, output,session) {
       list(
         src = "www/base_image.jpg",
         filetype = "image/jpg",
-        width = 1000,
-        height = 600,
+        width = 700,
+        height = 450,
         alt = "This is a factory"
       )
       
     }, deleteFile = FALSE)
     
-    predictions <- predict(object=model_trained, test_data[,0:59], type='prob')
-    
-    ID <- ids::random_id(length(predictions$anomaly), 4)
-    
-    result <- cbind(data.frame(ID),data.frame(predictions$anomaly))
-    
-    result$Status <- ifelse(as.numeric(result$predictions.anomaly) >=0.25, "Alert", "Normal")
-    
-    names(result)[names(result) == "predictions.anomaly"] <- "Probability"
-    
-    result$Probability <- round(result$Probability,2)
-    
-    result$Probability <- label_percent()(result$Probability)
-    
-    reval_omega <- reactiveVal(result[140,])
-    
-    reval_start <- reactiveVal(140)
-    
-    reval_end <- reactiveVal(140)
-    
-    reval_img <- reactiveVal("www/base_image.jpg")
+
     
     observeEvent(input$run, {
       
+      predictions <- predict(object=model_trained, test_data[,0:59], type='prob')
+      
+      ID <- ids::random_id(length(predictions$anomaly), 4)
+      
+      result <- cbind(data.frame(ID),data.frame(predictions$anomaly))
+      
+      result$Status <- ifelse(as.numeric(result$predictions.anomaly) >=(input$thres/100), "Alert", "Normal")
+      
+      names(result)[names(result) == "predictions.anomaly"] <- "Probability"
+      
+      result$Probability <- round(result$Probability,2)
+      
+      result$Probability <- label_percent()(result$Probability)
+      
+      reval_omega <- reactiveVal(result[510,])
+      
+      reval_start <- reactiveVal(510)
+      
+      reval_end <- reactiveVal(510)
+      
+      reval_img <- reactiveVal("www/base_image.jpg")
+      
       observe({
-        invalidateLater(500, session) 
+        invalidateLater(1000, session) 
         isolate({
           reval_start(reval_end()) 
           reval_end(reval_start() + 1) 
@@ -138,7 +146,7 @@ server <- function(input, output,session) {
                                           formatStyle(
                                             'Status',
                                             backgroundColor =  styleEqual(
-                                              unique(result$Status), c('green', 'red')
+                                              c('Normal', 'Alert'), c('green', 'red')
                                             )
                                           )
         )
